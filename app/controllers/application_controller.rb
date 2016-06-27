eee39382a9762d432ff76527172de8022bde5c74
jleon <jleon@webframe.cl>
2016-06-26 21:03:45 -0400

    user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+ 
   def after_sign_in_path_for(user)
-  	current_user_path #your path
+    current_user_path
   end
 
   def after_sign_in_path_for(admin)
