2c24d6cb3df88f37627c47d624be51ad573b9e1a
jleon <jleon@webframe.cl>
2016-06-27 22:51:52 -0400

    tops

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def current_user
-    return unless session[:user_id]
-    @current_user ||= User.find(session[:user_id])
-  end
-
   def after_sign_in_path_for(user)
     user_path(current_user)
   end
