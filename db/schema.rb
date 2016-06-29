73100023c0c4fa366ce359e2ebfd46c931290308
jleon <jleon@webframe.cl>
2016-06-29 18:25:58 -0400

    news

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,17 @@
-ActiveRecord::Schema.define(version: 20160629212305) do
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160629221645) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",              default: "", null: false
@@ -165,6 +178,11 @@ ActiveRecord::Schema.define(version: 20160629212305) do
     t.string   "logo_content_type"
     t.integer  "logo_file_size"
     t.datetime "logo_updated_at"
+    t.string   "newsletter"
+    t.string   "newsletter_file_name"
+    t.string   "newsletter_content_type"
+    t.integer  "newsletter_file_size"
+    t.datetime "newsletter_updated_at"
   end
 
   create_table "newsletters", force: :cascade do |t|
