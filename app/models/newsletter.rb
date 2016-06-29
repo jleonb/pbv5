ad5c43c4fd46926398b208a9f86b856c09af012e
jleon <jleon@webframe.cl>
2016-06-29 16:53:40 -0400

    news

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -1,4 +1,7 @@
 class Newsletter < ActiveRecord::Base
+
+	belongs_to :institucion
+	
 	has_attached_file :img, styles: { large: "500x1000>", thumb: "150x150#" }
   	validates_attachment_content_type :img, content_type: /\Aimage\/.*\Z/
 end
